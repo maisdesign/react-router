dcc581afb5b3f0cb20c51bc989c939cdb829070c
Maisdesign <downloadtaky@gmail.com>
2026-03-17 12:32:50 +0100

    Creata struttura di App e NavBar

--- react-router/src/App.jsx
+++ react-router/src/App.jsx
@@ -1,12 +1,19 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
+import DefaultLayout from "./layouts/DefaultLayout"
 import HomePage from "./pages/HomePage.jsx";
+import AboutUs from "./pages/AboutUs.jsx"
+import Products from "./pages/Products.jsx"
+
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<HomePage />} />
+        <Route element={<DefaultLayout />}>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/about-us" element={<AboutUs />} />
+        </Route>
       </Routes>
-    </BrowserRouter>
+    </BrowserRouter >
   );
 }
--- react-router/src/components/NavBar.jsx
+++ react-router/src/components/NavBar.jsx
@@ -1,6 +1,10 @@
+import { NavLink } from "react-router-dom";
 function NavBar() {
-    return <>
-    </>
+    return <nav>
+        <NavLink to="/" end>Home Page</NavLink>
+        <NavLink to="/products">Products</NavLink>
+        <NavLink to="/about-us">About Us</NavLink>
+    </nav>
 }
 
 export default NavBar
